08fda140d65de59d8480da3479c7394286d853d5
marcusmello <mandealista@gmail.com>
2021-05-17 20:25:40 -0300

    Bootsrap base and home done

--- loro/web/pages/items.py
+++ loro/web/pages/items.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-#from fastapi.staticfiles import StaticFiles
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-#app.mount("/static", StaticFiles(directory="loro/web/static"), name="static")
+app.mount("/static", StaticFiles(directory="loro/web/static"), name="static")
 templates = Jinja2Templates(directory="loro/web/templates")
 
 @app.get("/{id}", response_class=HTMLResponse)
