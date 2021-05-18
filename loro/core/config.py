cede6d7af91b19ee9c91f38d9feefde7d7c494f7
marcusmello <mandealista@gmail.com>
2021-05-18 16:20:26 -0300

    feat(database-mapping): add database providers credentials info

--- loro/core/config.py
+++ loro/core/config.py
@@ -1,16 +1,55 @@
-#pylint:disable=missing-module-docstring
-# pylint:disable=too-few-public-methods
-# pylint:disable=missing-class-docstring
+# pylint: skip-file
+
+import sys
+from typing import Union
 
 from environs import Env
-from pydantic import BaseSettings
+from pydantic import BaseModel, BaseSettings
 
+thismodule = sys.modules[__name__]
 env = Env()
+
+
+class Timesettings(BaseModel):
+    local_timezone = env.str("LOCAL_TIMEZONE", default="America/Bahia")
+    system_timezone = env.str("SYSTEM_TIMEZONE", default="UTC")
+    default_timestamp_unit = "s"
+    human_readable_format = "YYYY-MM-DD HH:mm:ss"
+    presented_in_human_readable_format = True
+
+
+class PostgresDatabase(BaseModel):
+    provider: str = "postgres"
+    sql_debug: bool = False
+    host: str = env.str("DB_HOST", default="localhost")
+    port: int = env.int("POSTGRES_PORT", default=5432)
+    database: str = env.str("POSTGRES_DB", default="postgres_db")
+    user: str = env.str("POSTGRES_USER", default="postgres_user")
+    password: str = env.str(
+        "POSTGRES_PASSWORD", default="123_postgres_password"
+    )
+
+
+class SqliteDatabase(BaseModel):
+    provider: str = "sqlite"
+    sql_debug: bool = False
+    filename: str = "database.sqlite"
+    create_db: bool = True
+
+
+def get_relational_database(
+    provider: str,
+) -> Union[PostgresDatabase, SqliteDatabase]:
+
+    return getattr(thismodule, "{}Database".format(provider.capitalize()))
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    domain = env.str("DOMAIN", default="http://127.0.0.1:8000")
+    domain: str = env.str("DOMAIN", default="http://127.0.0.1:8000")
+    relational_database: BaseModel = get_relational_database(
+        provider=env.str("RELATIONAL_DATABASE_PROVIDER", default="sqlite")
+    )
 
 
 settings = Settings()
