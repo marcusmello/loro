3ae995c6747682dbc2b79d9bb6bea95bd47880fd
marcusmello <mandealista@gmail.com>
2021-05-18 13:02:23 -0300

    refactor(web-routing): add 'domain' variable in order to adjust the relative routes

--- loro/core/config.py
+++ loro/core/config.py
@@ -1,8 +1,16 @@
-#from typing import Any, Dict, List, Optional, Union
+#pylint:disable=missing-module-docstring
+# pylint:disable=too-few-public-methods
+# pylint:disable=missing-class-docstring
 
+from environs import Env
 from pydantic import BaseSettings
+
+env = Env()
+
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
+    domain = env.str("DOMAIN", default="http://127.0.0.1:8000")
+
 
 settings = Settings()
