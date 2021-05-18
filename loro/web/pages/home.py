1f319b8ad446640d046fba77941a8fd268b601f8
marcusmello <mandealista@gmail.com>
2021-05-18 13:04:21 -0300

    refactor(web-routing): add 'domain' variable and 'home' path

--- loro/web/pages/home.py
+++ loro/web/pages/home.py
@@ -1,13 +1,27 @@
+# pylint:disable=missing-function-docstring
+# pylint:disable=missing-module-docstring
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-
 from fastapi.templating import Jinja2Templates
+from loro.core.config import settings
+
+DOMAIN = settings.domain
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="loro/web/static"), name="static")
 templates = Jinja2Templates(directory="loro/web/templates")
 
+
 @app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "domain": DOMAIN}
+    )
+
+@app.get("/home", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "domain": DOMAIN}
+    )
