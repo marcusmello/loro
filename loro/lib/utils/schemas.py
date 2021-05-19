e4ee5619ba0d88487085fd9d84c6e107b68192df
marcusmello <mandealista@gmail.com>
2021-05-19 11:33:36 -0300

    refactor(lib): remove the 'lib' package from .gitignore

---
+++ loro/lib/utils/schemas.py
@@ -0,0 +1,44 @@
+# pylint: skip-file
+
+from typing import List, Optional
+
+from pydantic import BaseModel, validator
+
+MESSAGES_DIRECTIONS = [
+    "incoming",
+    "outcoming",
+]
+
+
+class Choice(BaseModel):
+    text: str
+    leads_to: Optional[str] # Interaction (Dialog or Return) tag
+
+
+class Return(BaseModel):
+    tag: str
+    content: str
+
+
+class Dialog(BaseModel):
+    tag: str
+    header: str
+    choices: Optional[List[Choice]]
+    leads_to: Optional[str] # Interaction (Dialog or Return) tag
+
+
+class Message(BaseModel):
+    content: str
+    direction: str
+    timestamp: int
+
+    @validator("direction")
+    def direction_must_exist(cls, value):
+        if value not in MESSAGES_DIRECTIONS:
+            raise ValueError("invalid direction")
+        return value
+
+
+class FinalClient(BaseModel):
+    id: int
+    messages: List[Message]
