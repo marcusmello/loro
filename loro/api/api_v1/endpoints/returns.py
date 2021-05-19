a3e92640f40dfd08745b1704fbca8cd3f19c3472
marcusmello <mandealista@gmail.com>
2021-05-19 11:28:21 -0300

    refactor(api-routing): move 'db_session' to 'crud' and finalize necessary routes

--- loro/api/api_v1/endpoints/returns.py
+++ loro/api/api_v1/endpoints/returns.py
@@ -1,22 +1,26 @@
-from pony.orm import db_session
-
 from typing import List
 
-from fastapi import APIRouter, Request, HTTPException #, Response
+from fastapi import APIRouter, HTTPException
 from loro.lib.utils import schemas
-from loro.lib.utils.db.sql import crud, models
+from loro.lib.utils.db.sql import crud
 
 router = APIRouter()
 
 @router.post("/", response_model=schemas.Return)
-@db_session
 def create_return(return_: schemas.Return):
-    #db_return = crud.get_return_by_tag(tag=return_.tag)
-    #if db_return:
-    #    raise HTTPException(status_code=400, detail="Tag já existe, defina outra.")
+    return_in_db = crud.get_return_by_tag(tag=return_.tag)
+    if return_in_db:
+        raise HTTPException(status_code=400, detail="Tag já existe, defina outra.")
     return crud.create_return(return_=return_)
 
 @router.get("/", response_model=List[schemas.Return])
 def read_returns(limit: int = 100):
     returns = crud.get_returns(limit=limit)
     return returns
+
+@router.get("/{tag}", response_model=schemas.Return)
+def read_return_by_tag(tag: str):
+    return_in_db = crud.get_return_by_tag(tag=tag)
+    if not return_in_db:
+        raise HTTPException(status_code=400, detail="Tag inexistente")
+    return return_in_db
