10f62005ee3eee3dac410fdabe71fc84654cc38d
marcusmello <mandealista@gmail.com>
2021-05-21 13:49:28 -0300

    rebuild the web routes

--- loro/main.py
+++ loro/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 from loro.api.api_v1.api import api_router
 from loro.core.config import settings
-from loro.web.pages import router
+from loro.web.routes import router
 
 app = FastAPI()
 app.include_router(api_router, prefix=settings.API_V1_STR)
@@ -11,5 +11,5 @@ app.mount('', router.app)
 
 
 def start():
-    """Launched with `poetry run start` at root level"""
+    """Launched with `poetry run loro` at root level"""
     uvicorn.run("loro.main:app", host="0.0.0.0", port=8000, reload=True)
