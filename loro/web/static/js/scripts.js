44c33a4d9adbfbc7629ba807000854dc7a929cd7
marcusmello <mandealista@gmail.com>
2021-05-24 18:34:12 -0300

    pass dynamic url through modal

--- loro/web/static/js/scripts.js
+++ loro/web/static/js/scripts.js
@@ -75,3 +75,15 @@
     });
 
 })(jQuery); // End of use strict
+
+// Modal interaction with dynamic href button
+$(document).ready(function(){
+    $("#confirmDelete").on("show.bs.modal", function(event){
+        // Get the button that triggered the modal
+        var button = $(event.relatedTarget);
+
+        // Extract value from the custom data-* attribute
+        var url = button.data("url");
+        $(this).find('#confirm-delete').attr('href', url)
+    });
+});
