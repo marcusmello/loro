c8e18582d0ea88c6de1360fed5c5d263c12435d2
marcusmello <mandealista@gmail.com>
2021-05-24 18:33:57 -0300

    implemet 'update' and 'delete' methods and rename existen methods

--- loro/lib/utils/db/sql/crud/returns.py
+++ loro/lib/utils/db/sql/crud/returns.py
@@ -5,12 +5,12 @@ from pony.orm import db_session
 
 
 @db_session
-def create_return(return_: schemas.Return):
+def create(return_: schemas.Return):
     _return = models.Return(tag=return_.tag, content=return_.content)
     return _return.to_dict()
 
 @db_session
-def get_return_by_tag(tag: str):
+def read(tag: str):
     try:
         return_in_db = models.Return.get(tag=tag)
         return return_in_db.to_dict()
@@ -18,7 +18,19 @@ def get_return_by_tag(tag: str):
         return dict()
 
 @db_session
-def get_returns(limit:int):
+def update(tag: str, new_return: schemas.Return):
+    return_in_db = models.Return.get(tag=tag)
+    return_in_db.update(**new_return.dict())
+    return return_in_db.to_dict()
+
+@db_session
+def delete(tag: str):
+    return_in_db = models.Return.get(tag=tag)
+    return_in_db.delete()
+    return dict(tag="", content= "")
+
+@db_session
+def get(limit: int):
     try:
         return [r.to_dict() for r in models.Return.select()[:limit]]
     except:
