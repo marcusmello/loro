2b8f31ccc481be20ff745a9eb96f06283f865407
marcusmello <mandealista@gmail.com>
2021-05-24 18:33:52 -0300

    implement 'update' method and rename existent methods

--- loro/api/api_v1/endpoints/returns.py
+++ loro/api/api_v1/endpoints/returns.py
@@ -1,26 +1,43 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Form
 from loro.lib.utils import schemas
-from loro.lib.utils.db.sql import crud
+from loro.lib.utils.db.sql.crud import returns
 
 router = APIRouter()
 
-@router.post("/", response_model=schemas.Return)
-def create_return(return_: schemas.Return):
-    return_in_db = crud.get_return_by_tag(tag=return_.tag)
-    if return_in_db:
-        raise HTTPException(status_code=400, detail="Tag já existe, defina outra.")
-    return crud.create_return(return_=return_)
 
-@router.get("/", response_model=List[schemas.Return])
-def read_returns(limit: int = 100):
-    returns = crud.get_returns(limit=limit)
-    return returns
+@router.post("/", response_model=schemas.Return)
+def create(tag: str = Form(...), content: str = Form(...)):
+    return_in_db = returns.read(tag=tag)
+    if return_in_db:
+        raise HTTPException(
+            status_code=400, detail="Tag já existe, defina outra."
+        )
+    return returns.create(return_=schemas.Return(
+            tag=tag,
+            content=content,
+        ))
+
 
 @router.get("/{tag}", response_model=schemas.Return)
-def read_return_by_tag(tag: str):
-    return_in_db = crud.get_return_by_tag(tag=tag)
+def read(tag: str):
+    return_in_db = returns.read(tag=tag)
     if not return_in_db:
         raise HTTPException(status_code=400, detail="Tag inexistente")
     return return_in_db
+
+
+@router.put("/{tag}", response_model=schemas.Return)
+def update(tag: str, new_return: schemas.Return):
+    return returns.update(tag=tag, new_return=new_return)
+
+
+@router.delete("/{tag}", response_model=schemas.Return)
+def delete(tag: str):
+    return returns.delete(tag=tag)
+
+
+@router.get("/", response_model=List[schemas.Return])
+def get(limit: int = 100):
+    return returns.get(limit=limit)
