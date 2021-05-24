0a93bf27a0f9fece9529fd954a771d024fae414b
marcusmello <mandealista@gmail.com>
2021-05-24 18:34:06 -0300

    import 'Form', 'status' and 'RedirectResponse'; delete 'crud' import

--- loro/web/routes/router.py
+++ loro/web/routes/router.py
@@ -2,12 +2,12 @@
 # pylint:disable=missing-module-docstring
 # pylint:disable=unused-import
 
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Form, Request, status
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 #from loro.core.config import settings
-from loro.lib.utils.db.sql import crud
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="loro/web/static"), name="static")
