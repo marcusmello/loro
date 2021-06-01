aabcf18a0a22207030aa454d89f3c1578949a734
marcusmello <mandealista@gmail.com>
2021-06-01 18:28:02 -0300

    feat(web-routing): add update routes

--- loro/web/pages/dialogs.py
+++ loro/web/pages/dialogs.py
@@ -4,21 +4,23 @@ from loro.core.config import settings
 from loro.lib.utils.db.sql.crud import dialogs
 
 from .dialogs_dynamic_form import (
-    DynamicForm,
+    DynamicContext,
     ParseForm,
 )
 from .router import (
     HTMLResponse,
-    # RedirectResponse,
+    RedirectResponse,
     Request,
     app,
-    # status,
+    status,
     templates,
     HTTPException,
 )
 
 paths = settings.url_paths.dialogs
-dynamic_form = DynamicForm()
+
+DYNAMIC_FORM = "dialogs/dynamic_form.html"
+dynamic_context = DynamicContext()
 parse_form = ParseForm()
 
 
@@ -40,8 +42,8 @@ def get(request: Request, limit: int = 100):
 @app.get(paths.create, response_class=HTMLResponse)
 def create(request: Request):
     return templates.TemplateResponse(
-        "dialogs/dynamic_form.html",
-        context=dynamic_form.empty_create(request),
+        DYNAMIC_FORM,
+        context=dynamic_context.default_create(request),
     )
 
 
@@ -50,9 +52,12 @@ async def create(request: Request):
     form = await request.form()
     dialog = parse_form.from_raw_form(dialog_form=form.multi_items())
 
+    if not (dialog.tag and dialog.header):
+        raise HTTPException(status_code=502)
+
     tag_already_exists = dialogs.read_dialog(tag=dialog.tag)
     if tag_already_exists:
-        raise HTTPException(status_code=400)
+        raise HTTPException(status_code=403)
 
     dialogs.create_dialog(dialog)
 
@@ -62,6 +67,45 @@ def invalid_create(request: Request, data: str):
     dialog = parse_form.from_raw_invalid_input(data=data)
 
     return templates.TemplateResponse(
-        "dialogs/dynamic_form.html",
-        context=dynamic_form.invalid_create(request, dialog),
+        DYNAMIC_FORM,
+        context=dynamic_context.invalid_create(request, dialog),
     )
+
+@app.get(path=paths.update + "/{tag}", response_class=HTMLResponse)
+def update(request: Request, tag:str):
+    dialog_to_update = dialogs.read_dialog(tag=tag)
+
+    return templates.TemplateResponse(
+        DYNAMIC_FORM,
+        context=dynamic_context.default_update(request, tag, dialog_to_update),
+    )
+
+@app.post(path=paths.update + "/{tag}")
+async def update(request: Request, tag:str):
+    form = await request.form()
+    new_dialog = parse_form.from_raw_form(dialog_form=form.multi_items())
+
+    if not (new_dialog.tag and new_dialog.header):
+        raise HTTPException(status_code=502)
+
+    if new_dialog.tag != tag:
+        tag_already_exists = dialogs.read_dialog(tag=new_dialog.tag)
+        if tag_already_exists:
+            raise HTTPException(status_code=403)
+
+    dialogs.update_dialog(tag=tag, new_dialog=new_dialog)
+
+
+@app.get(path=paths.update_error + "/{tag}/{data}")
+def invalid_update(request: Request, tag: str, data: str):
+    dialog_to_update = parse_form.from_raw_invalid_input(data=data)
+
+    return templates.TemplateResponse(
+        DYNAMIC_FORM,
+        context=dynamic_context.invalid_update(request, tag, dialog_to_update),
+    )
+
+@app.get(path=paths.delete + "/{tag}", response_class=HTMLResponse)
+def delete(tag: str):
+    dialogs.delete_dialog(tag=tag)
+    return RedirectResponse(url=paths.root, status_code=status.HTTP_303_SEE_OTHER)
