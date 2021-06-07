41309a0bd26b841316cae0e2d4c91586c327ecf1
marcusmello <mandealista@gmail.com>
2021-06-07 20:17:45 -0300

    bring auth token from serrings and refactor cookie logic

--- loro/api/api_v1/endpoints/twilio_hook.py
+++ loro/api/api_v1/endpoints/twilio_hook.py
@@ -1,39 +1,52 @@
-#from typing import Optional
+# from typing import Optional
 
 from fastapi import APIRouter, Form, HTTPException, Request, Response
 from twilio.request_validator import RequestValidator
 from twilio.twiml.messaging_response import MessagingResponse
+from loro.lib.chatbot import ChatFlowHandler
+from loro.core.config import settings
 
+TWILIO_AUTH_TOKEN = settings.twilio.auth_token
 router = APIRouter()
+
 
 @router.post("/")
 async def chat(request: Request, From: str = Form(...), Body: str = Form(...)):
+    twilio_response = MessagingResponse()
+    response_message = twilio_response.message()
 
-#    validator = RequestValidator("e55746c4f1e1b2ba94125f9679cb270e")#os.environ["TWILIO_AUTH_TOKEN"])
-    access_counter = int(request.cookies.get("session", 0))
-    access_counter += 1
+    validator = RequestValidator(TWILIO_AUTH_TOKEN)
+    twilio_incoming_form = await request.form()
 
-    form_ = await request.form()
-
-#    if not validator.validate(
-#        str(request.url),
-#        form_,
-#        request.headers.get("X-Twilio-Signature", "")
-#    ):
-#        raise HTTPException(status_code=400, detail="Error in Twilio Signature")
-
-    msg_ = (
-        """\nAccess_number: {}\n From: {}\n Body: {}\n form_: {}\n """.format(
-            access_counter, From, Body, form_
+    if not validator.validate(
+        str(request.url),
+        twilio_incoming_form,
+        request.headers.get("X-Twilio-Signature", ""),
+    ):
+        raise HTTPException(
+            status_code=400, detail="Error in Twilio Signature"
         )
+
+    answer_sequence_cookie = request.cookies.get("answerSequence", str())
+
+    print("cookie: ", answer_sequence_cookie)
+    print("form type: ", type(twilio_incoming_form))
+
+    chat_flow = ChatFlowHandler(
+        from_user=From,
+        form=twilio_incoming_form,
     )
+    chat_flow.run(
+        incoming_msg=Body, answer_sequence_string=answer_sequence_cookie
+    )
+    response_message.body(chat_flow.response)
 
-    resp = MessagingResponse()
-    msg = resp.message()
-    msg.body(msg_)
-
-    response = Response(content=str(resp), media_type="application/xml")
+    response = Response(
+        content=str(twilio_response), media_type="application/xml"
+    )
     response.set_cookie(
-        key="session", expires=30, httponly=True, value=str(access_counter)
+        key="answerSequence",
+        httponly=True,
+        value=chat_flow.answer_sequence.string,
     )
     return response
