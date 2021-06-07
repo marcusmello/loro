2e7f58fc5cd2d77f055c0a52c701d6cd9f839537
marcusmello <mandealista@gmail.com>
2021-06-07 20:17:42 -0300

    bring routes prefixes from settings

--- loro/api/api_v1/api.py
+++ loro/api/api_v1/api.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter
 from loro.api.api_v1.endpoints import twilio_hook, answers
+from loro.core.config import settings
 
 api_router = APIRouter()
-api_router.include_router(twilio_hook.router, prefix="/twilio-hook")
-api_router.include_router(answers.router, prefix="/answers")
+
+api_router.include_router(
+    twilio_hook.router, prefix=settings.url_paths.twilio_hook
+)
+api_router.include_router(
+    answers.router, prefix=settings.url_paths.answers.root
+)
