29d1f40a7961c51b3cbbad1e3e9d4875ec88abb7
marcusmello <mandealista@gmail.com>
2021-06-07 20:17:48 -0300

    implement twiilio configurations parameters, cors origins, default choices and wellcome answer

--- loro/core/config.py
+++ loro/core/config.py
@@ -10,11 +10,20 @@ thismodule = sys.modules[__name__]
 env = Env()
 
 
+class TwilioParameters(BaseModel):
+    auth_token = env.str(
+        "TWILIO_AUTH_TOKEN", default="e55746c4f1e1b2ba94125f9679cb270e"
+    )
+    callback_hook_path = "/twilio-hook"
+
+
 class Timesettings(BaseModel):
     local_timezone = env.str("LOCAL_TIMEZONE", default="America/Bahia")
     system_timezone = env.str("SYSTEM_TIMEZONE", default="UTC")
-    default_timestamp_unit = "s"
-    human_readable_format = "YYYY-MM-DD HH:mm:ss"
+    default_timestamp_unit = env.str("DEFAULT_TIMESTAMP_UNIT", default="s")
+    human_readable_format = env.str(
+        "HUMAN_READABLE_FORMAT", default="YYYY-MM-DD HH:mm:ss"
+    )
     presented_in_human_readable_format = True
 
 
@@ -71,27 +80,98 @@ answers_paths.generate()
 
 class UrlPaths(BaseModel):
     answers = answers_paths
+    twilio_hook = TwilioParameters().callback_hook_path
+    API_prefix_version: str = "/api/v1"
 
 
-class answersDynamicFormVariables(BaseModel):
+class CorsOrigins(BaseModel):
+    paths: dict = UrlPaths().dict()
+    protocols: list = ["http", "https"]
+    main_domain: str = "localhost"
+    main_port: int = 8000
+
+    domains: list = (
+        ",{},".format(main_domain).join(["127.0.0.1", "0.0.0.0"])
+    ).split(",")
+
+    ports: list = (",{},".format(main_domain).join(["80", "443"])).split(",")
+
+    def generate(self):
+        all_paths = []
+        all_paths = [
+            r[1]
+            for r in [route.items() for route in self.paths.values()][0]
+            if r[1] not in all_paths
+        ]
+        simple_urls = [
+            "{}://{}{}".format(protocol, domain, path)
+            for protocol in self.protocols
+            for domain in self.domains
+            for path in all_paths
+        ]
+        port_urls = [
+            "{}://{}:{}{}".format(protocol, domain, port, path)
+            for protocol in self.protocols
+            for domain in self.domains
+            for port in self.ports
+            for path in all_paths
+        ]
+
+        return simple_urls + port_urls
+
+
+class AnswersDynamicFormVariables(BaseModel):
     default_empty_tag: str = "Tag Seguinte"
 
 
 class WebTemplatingVariables(BaseModel):
-    answers_dynamic_forms = answersDynamicFormVariables()
+    answers_dynamic_forms = AnswersDynamicFormVariables()
+
+
+def possible_variation(string: str):
+    return [
+        string,
+        string.capitalize(),
+        string.upper(),
+        string.lower(),
+        string.swapcase(),
+        string.casefold(),
+    ]
+
+
+class DefaultChoices(BaseModel):
+    back_choice: str = "voltar"
+    exit_choice: str = "sair"
+
+    back_choices: list = possible_variation(string=back_choice)
+    exit_choices: list = possible_variation(string=exit_choice)
+
+    back_choice_message = (
+        """Digite "{}" para retornar ao menu anterior\n""".format(back_choice)
+    )
+    exit_choice_message = """Digite {} para encerrar o atendimento""".format(
+        exit_choice
+    )
+
+
+class WellcomeAnswer(BaseModel):
+    tag = "welcome"
+    header = "Olá mundo"
 
 
 class Settings(BaseSettings):
     class Config:
         arbitrary_types_allowed = True
 
-    API_V1_STR: str = "/api/v1"
     relational_database: BaseModel = get_relational_database(
         provider=env.str("RELATIONAL_DATABASE_PROVIDER", default="sqlite")
     )
     sql_debug: bool = False
     url_paths = UrlPaths()
     web_templating_variables = WebTemplatingVariables()
+    cors_origins = CorsOrigins()
+    default_choices = DefaultChoices()
+    wellcome = WellcomeAnswer()
 
 
 settings = Settings()
