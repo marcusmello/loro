8bc6fbf49be6c088d107a57b36ea8ef758e4ef29
marcusmello <mandealista@gmail.com>
2021-06-09 16:10:11 -0300

    fix(API): fix route variable

--- loro/api/api_v1/api.py
+++ loro/api/api_v1/api.py
@@ -5,7 +5,7 @@ from loro.core.config import settings
 api_router = APIRouter()
 
 api_router.include_router(
-    twilio_hook.router, prefix=settings.url_paths.twilio_hook
+    twilio_hook.router, prefix=settings.twilio.callback_hook_path
 )
 api_router.include_router(
     answers.router, prefix=settings.url_paths.answers.root
